0790e2a04d891483de0ecdfe4481da59be716907
Ananya015 <jainananya78@gmail.com>
2020-12-24 15:14:44 +0530

    initializing array

--- src/Array/Kadane_algorithm.java
+++ src/Array/Kadane_algorithm.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
 public class Kadane_algorithm {
     public static void main(String[] args) {
         Scanner scanner=new Scanner ( System.in );
-
+        System.out.println ("Enter size of array");
+        int size_of_array= scanner.nextInt ();
+        System.out.println ("Enter elements of array");
+        int array[] = new int[size_of_array];
+        for(int i=0;i<size_of_array;i++){
+            array[i] = scanner.nextInt ();
+        }
     }
 
 }
