859c320e4476ff24220df10c8c55369e009070a0
Ananya015 <jainananya78@gmail.com>
2020-12-24 15:10:53 +0530

    adding scanner class

--- src/Array/Kadane_algorithm.java
+++ src/Array/Kadane_algorithm.java
@@ -1,4 +1,11 @@
 package Array;
 
+import java.util.Scanner;
+
 public class Kadane_algorithm {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner ( System.in );
+
+    }
+
 }
