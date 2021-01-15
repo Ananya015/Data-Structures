ba3d59a271eafdf6a6c0d72981c9e149d503d102
Ananya015 <jainananya78@gmail.com>
2021-01-15 23:17:45 +0530

    adding print function

--- src/LinkedList/MainLinkedList.java
+++ src/LinkedList/MainLinkedList.java
@@ -1,4 +1,13 @@
 package LinkedList;
 
+import java.util.List;
+
 public class MainLinkedList {
+    public static void main(String[] args) {
+
+        SinglyLinkedList<Integer> myLL= new SinglyLinkedList<> ();
+        myLL.add ( 89 );
+        myLL.add ( 44 );
+        myLL.print ();
+    }
 }
--- src/LinkedList/SinglyLinkedList.java
+++ src/LinkedList/SinglyLinkedList.java
@@ -2,6 +2,31 @@ package LinkedList;
 
 public class SinglyLinkedList<E> {
 
+    Node<E> head;
+
+    void add(E data){
+        Node toAdd = new Node ( data );
+
+        if(head==null) {
+            head=toAdd;
+            return;
+        }
+        Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=toAdd;
+    }
+
+    void print(){
+        Node temp=head;
+        while (temp!=null){
+            System.out.println (temp.data +" ");
+            temp=temp.next;
+        }
+    }
+
+
     static class Node<E>{
        E data;
        Node<E> next;
@@ -9,7 +34,6 @@ public class SinglyLinkedList<E> {
         public Node( E data){
             this.data=data;
             next=null;
-
         }
     }
 
