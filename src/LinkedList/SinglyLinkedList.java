0aaaa54854bd5fd3abc57aa27262aa608ab686e6
Ananya015 <jainananya78@gmail.com>
2021-01-15 23:00:11 +0530

    adding node class

--- src/LinkedList/SinglyLinkedList.java
+++ src/LinkedList/SinglyLinkedList.java
@@ -1,5 +1,16 @@
 package LinkedList;
 
-public class SinglyLinkedList {
+public class SinglyLinkedList<E> {
+
+    static class Node<E>{
+       E data;
+       Node<E> next;
+
+        public Node( E data){
+            this.data=data;
+            next=null;
+
+        }
+    }
 
 }
