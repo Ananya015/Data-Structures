57d82b048c92adebf5dc4e0c79bbbd1ddb638c6d
Ananya015 <jainananya78@gmail.com>
2021-01-16 20:02:53 +0530

    adding atposition function

--- src/LinkedList/MainLinkedList.java
+++ src/LinkedList/MainLinkedList.java
@@ -6,8 +6,10 @@ public class MainLinkedList {
     public static void main(String[] args) {
 
         SinglyLinkedList<Integer> myLL= new SinglyLinkedList<> ();
-        myLL.add ( 89 );
         myLL.add ( 44 );
+        myLL.add ( 89 );
+        myLL.add ( 49 );
+        myLL.atpostion ( 33,2 );
         myLL.print ();
     }
 }
--- src/LinkedList/SinglyLinkedList.java
+++ src/LinkedList/SinglyLinkedList.java
@@ -3,10 +3,9 @@ package LinkedList;
 public class SinglyLinkedList<E> {
 
     Node<E> head;
-
+ //adding new node
     void add(E data){
         Node toAdd = new Node ( data );
-
         if(head==null) {
             head=toAdd;
             return;
@@ -18,6 +17,19 @@ public class SinglyLinkedList<E> {
         temp.next=toAdd;
     }
 
+    void atpostion( E data,int  position){
+        Node toAdd= new Node (data);
+        Node temp=head;
+        int index=1;
+        while(index<position-1){
+            temp=temp.next;
+            index++;
+        }
+        toAdd.next=temp.next;
+        temp.next=toAdd;
+
+    }
+// printing linkedList
     void print(){
         Node temp=head;
         while (temp!=null){
@@ -26,12 +38,11 @@ public class SinglyLinkedList<E> {
         }
     }
 
-
+//node class
     static class Node<E>{
        E data;
        Node<E> next;
-
-        public Node( E data){
+       public Node( E data){
             this.data=data;
             next=null;
         }
